aa69674eb5a765ccd36109e338c62a5e58509bd2
rik cousens <rcousens7@comcast.net>
2017-12-18 05:12:41 -0500

    created reference to API key

--- blah.bat
+++ blah.bat
@@ -7,3 +7,4 @@ if not exist "%DEST%" md "%DEST%"
 csc -nologo %NAME%.cs -out:%DLL% 
 nuget spec -force -a %DLL% -verbosity detailed
 REM REM nuget pack  -verbosity detailed %NAME%.nuspec 	
+ nuget pack  -verbosity detailed  test.nuspec -properties owners=janedoe,harikm,kimo,xiaop;desc="Awesome app logger utility";tags=riktest 
